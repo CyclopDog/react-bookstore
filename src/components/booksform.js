4ea30a702b69443d29ec5a501697b65899d6d2f5
Marcos A. Fernandes <contato@marcos-fernandes.com>
2019-11-07 15:20:40 -0300

    can add books to store

--- src/components/booksform.js
+++ src/components/booksform.js
@@ -1,17 +1,38 @@
 import React from 'react'
+import { createBook } from '../actions/index'
+import { connect } from 'react-redux'
 
-const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
+class BooksForm extends React.Component {
+  constructor(props){
+    super(props)
 
-export default class Booksform extends React.Component {
+    this.state = {
+      title: '',
+      category: 'Action'
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    this.props.createBook(this.state)
+    this.setState({ title: '' })
+  }
+
   render() {
+    const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
     const cats = categories.map(cat => {
       return <option value={cat}>{cat}</option>
     })
     
     return (
-    <form>
-      <input type='text'></input>
-      <select>
+    <form onSubmit={this.handleSubmit}>
+      <input type='text' name='title' value={this.state.title} onChange={this.handleChange}></input>
+      <select name='category' value={this.state.category} onChange={this.handleChange}>
         {cats}
       </select>
       <button>Submit</button>
@@ -19,3 +40,5 @@ export default class Booksform extends React.Component {
     )
   }
 }
+
+export default connect(null, {createBook: createBook})(BooksForm)
