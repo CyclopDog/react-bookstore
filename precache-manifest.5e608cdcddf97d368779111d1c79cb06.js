0830044541803b60ffe60f74c0fbbebffc451dac
Marcos A. Fernandes <contato@marcos-fernandes.com>
2019-11-19 16:10:36 -0300

    Updates

---
+++ precache-manifest.5e608cdcddf97d368779111d1c79cb06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354a3860653750b94ad5fe5c843ee236",
+    "url": "/react-bookstore/index.html"
+  },
+  {
+    "revision": "bcd7576709a6d7ef1207",
+    "url": "/react-bookstore/static/css/main.429ff748.chunk.css"
+  },
+  {
+    "revision": "8293aff518776286e0b0",
+    "url": "/react-bookstore/static/js/2.34fde8f3.chunk.js"
+  },
+  {
+    "revision": "bcd7576709a6d7ef1207",
+    "url": "/react-bookstore/static/js/main.1b0f8499.chunk.js"
+  },
+  {
+    "revision": "62965ec85482588b87bb",
+    "url": "/react-bookstore/static/js/runtime-main.dec2baaf.js"
+  }
+]);
